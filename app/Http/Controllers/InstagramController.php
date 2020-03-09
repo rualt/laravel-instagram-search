ed3ff6ca2a9cbb3f0b1dab99de7e0481fa2bae9b
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-10 02:02:11 +0400

    add errors catch in controller and displaying it in view

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -3,19 +3,40 @@
 namespace App\Http\Controllers;
 
 use InstagramScraper\Instagram;
+use InstagramScraper\Exception\InstagramNotFoundException;
 use Illuminate\Http\Request;
 
 class InstagramController extends Controller
 {
 
-    public function home()
-    {
+    public function home(Request $request)
+    {   
+        $error = '';
         $instagram = new Instagram();
-            $tag = 'cats';
-            $medias = $instagram->getMediasByTag($tag, 44);
-            foreach ($medias as $media) {
-                $images[] = $media->getImageHighResolutionUrl();
+        $tag = $request['tag'];
+        $tag = str_replace(' ', '', $tag);
+                
+        try {
+            $medias = $instagram->getMediasByTag($tag);
+            if (!isset($medias[0])) {
+                throw new \Exception('You should probably be logged in to search for this. Or no results.');
             }
+        } catch (InstagramNotFoundException $exception) {
+            $error = $exception->getMessage();
+        } catch (\Exception $exception) {
+            $error = $exception->getMessage();
+        }
+
+        if (!empty($error)) {
+            return view('welcome', ['tag' => $tag, 'error' => $error]);
+        }
+        
+        $medias = $instagram->getMediasByTag($tag, 44);
+
+        foreach ($medias as $media) {
+            $images[] = $media->getImageHighResolutionUrl();
+        }
+
         $params = [
             'tag' => $tag,
             'images' => $images
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-         <h2>Search instagram by tag</h2>
-        <form action="index.php" method="get">
-            <p><input type="search" name='tag' value="{{$tag ?? ''}}" placeholder="Tag"> 
-                <input type="submit" value="Search">
-            </p>
-        </form>
-        <div id="_container-images">
+    <h2>Search instagram by tag</h2>
+    <form action="{{ action('InstagramController@home') }}" method="get">
+        <p><input type="search" name='tag' value="{{$tag}}" placeholder="Tag"> 
+            <input type="submit" value="Search">
+        </p>
+    </form>
+
+    <div id="_container-images">
+        @if (empty($tag))
+            <p>Try to search something</p>
+        @elseif (isset($error))
+            <p>{{ $error }}</p>
+        @else
             @foreach ($images as $image)
                 <img src="{{ $image }}">
             @endforeach
-        </div>
+        @endif
+
+    </div>
 @endsection
