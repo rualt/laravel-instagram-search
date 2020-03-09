70c0b54b8f0754983ecf581c0fd9f7ebcf14b28f
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-09 23:06:20 +0400

    add to the welcome page cats pictures output from instagram

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -5,28 +5,22 @@ namespace App\Http\Controllers;
 use InstagramScraper\Instagram;
 use Illuminate\Http\Request;
 
-class ArticleController extends Controller
+class InstagramController extends Controller
 {
 
     public function home()
     {
         $instagram = new Instagram();
-
-        if (!empty($_REQUEST['tag'])) {
-            $tag = htmlspecialchars($_REQUEST['tag']);
+            $tag = 'cats';
             $medias = $instagram->getMediasByTag($tag, 44);
             foreach ($medias as $media) {
                 $images[] = $media->getImageHighResolutionUrl();
             }
-        } else {
-            $tag = '';
-        }
-
         $params = [
             'tag' => $tag,
             'images' => $images
         ];
 
-        return view('instagram.welcome', compact('params'));
+        return view('welcome', $params);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <h2>Search instagram by tag</h2>
+@section('content')
+         <h2>Search instagram by tag</h2>
         <form action="index.php" method="get">
-            <p><input type="search" name='tag' value="{{ tag }}" placeholder="Tag"> 
+            <p><input type="search" name='tag' value="{{$tag ?? ''}}" placeholder="Tag"> 
                 <input type="submit" value="Search">
             </p>
         </form>
-            <p>
-            {% if images is defined %}
-                <div id="_container-images">
-                    {% for image in images %}
-                        <img src="{{image}}">
-                    {% endfor %}
-                </div>
-            {% endif %}
-    </body>
-</html>
+        <div id="_container-images">
+            @foreach ($images as $image)
+                <img src="{{ $image }}">|
+            @endforeach
+        </div>
+@endsection
