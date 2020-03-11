c697abfe3604533c61c8965b04bee78363716b39
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-11 22:08:13 +0400

    fix check for errors and emty tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<main role="main">
-  <section class="jumbotron text-center">
-  <div class="container">
-    <h1>InstaTagSearch</h1>
-    <p class="lead text-muted">Search instagram by tag</p>
-    <form action="{{ action('InstagramController@tagSearch') }}" method="get">
-        <p>
-          <input type="search" name='tag' value="{{$tag}}" placeholder="Tag"> 
-          <input type="submit" value="Search">
-        </p>
-    </form>
-  </div>
-</section>
-  <div class="album py-5 bg-light">
-  <div class="container">
-    <div class="row">
-      @if (empty($tag))
-        <p>Try to search something</p>
-      @elseif (isset($error))
-        <p>{{ $error }}</p>
-      @else
-        @foreach ($images as $image)
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <div class="instagram-image">
-                  <img src="{{ $image }}">
-              </div>
-            </div> 
-          </div>
-        @endforeach
-      @endif
-        </div>
+    <main role="main">
+    <section class="jumbotron text-center">
+    <div class="container">
+        <h1>InstaTagSearch</h1>
+        <p class="lead text-muted">Search instagram by tag</p>
+        <form action="{{ action('InstagramController@tagSearch') }}" method="get">
+            <p>
+            <input type="search" name='tag' value="{{$tag}}" placeholder="Tag"> 
+            <input type="submit" value="Search">
+            </p>
+        </form>
     </div>
-  </div>
+    </section>
+
+    @if (isset($error) and !empty($tag))
+        <p>{{ $error }}</p>
+    @elseif (!empty($tag))
+        <div class="album py-5 bg-light">
+            <div class="container">
+                <div class="row">
+                    @foreach ($images as $image)
+                        <div class="col-md-4">
+                            <div class="card mb-4 shadow-sm">
+                                <div class="instagram-image">
+                                    <img src="{{ $image }}">
+                                </div>
+                            </div> 
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    @endif
 </main>
 @endsection
