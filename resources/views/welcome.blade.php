96e032c1edd551404f40090ca02b7309bae3817b
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-11 22:17:11 +0400

    add style to the error message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,22 @@
 @section('content')
     <main role="main">
     <section class="jumbotron text-center">
-    <div class="container">
-        <h1>InstaTagSearch</h1>
-        <p class="lead text-muted">Search instagram by tag</p>
-        <form action="{{ action('InstagramController@tagSearch') }}" method="get">
-            <p>
-            <input type="search" name='tag' value="{{$tag}}" placeholder="Tag"> 
-            <input type="submit" value="Search">
-            </p>
-        </form>
-    </div>
+        <div class="container">
+            <h1>InstaTagSearch</h1>
+            <p class="lead text-muted">Search instagram by tag</p>
+            <form action="{{ action('InstagramController@tagSearch') }}" method="get">
+                <p>
+                <input type="search" name='tag' value="{{$tag}}" placeholder="Tag"> 
+                <input type="submit" value="Search">
+                </p>
+            </form>
+        </div>
     </section>
 
     @if (isset($error) and !empty($tag))
+    <section class="font-weight-bold text-center text-muted">
         <p>{{ $error }}</p>
+    </section>
     @elseif (!empty($tag))
         <div class="album py-5 bg-light">
             <div class="container">
