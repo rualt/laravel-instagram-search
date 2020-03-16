c84292ca569508fc8fa54bd43e0e7f703124918f
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-16 23:18:53 +0400

    add imageCount variable from form options

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -10,14 +10,13 @@ class InstagramController extends Controller
 {
 
     public function tagSearch(Request $request)
-    {   
+    {
         $error = '';
         $instagram = new Instagram();
-        $tag = $request['tag'];
-        $tag = str_replace(' ', '', $tag);
-                
+        $tag = str_replace(' ', '', $request['tag']);
+        $imageCount = (int) $request['imageCount'];
         try {
-            $medias = $instagram->getMediasByTag($tag);
+            $medias = $instagram->getMediasByTag($tag, $imageCount);
             if (!isset($medias[0])) {
                 throw new \Exception('You should probably be logged in to search for this. Or no results.');
             }
@@ -26,19 +25,26 @@ class InstagramController extends Controller
         } catch (\Exception $exception) {
             $error = $exception->getMessage();
         }
+
         if (!empty($error)) {
             return view('welcome', ['tag' => $tag, 'error' => $error]);
         }
 
-        $medias = $instagram->getMediasByTag($tag, 44);
+        $medias = $instagram->getMediasByTag($tag, $imageCount);
         foreach ($medias as $media) {
-            $images[] = $media->getImageHighResolutionUrl();
+            $images[] = [
+                'original' => $media->getImageHighResolutionUrl(),
+                'square' => $media->getSquareImages()[4],
+                'caption' => $media->getCaption(),
+                'link' => $media->getLink()
+            ];
         }
         $params = [
             'tag' => $tag,
-            'images' => $images
+            'images' => $images,
+            'media' => $medias[0]
         ];
 
-        return view('welcome', $params);
+        return view('pages.index', $params);
     }
 }
