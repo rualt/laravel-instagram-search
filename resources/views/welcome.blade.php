489daac483bb6bf689875c0714dfb910fbc02689
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-16 23:18:19 +0400

    change form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,29 @@
         <div class="container">
             <h1>InstaTagSearch</h1>
             <p class="lead text-muted">Search instagram by tag</p>
-            <form class="form-inline align-items-center" action="{{ action('InstagramController@tagSearch') }}" method="get">
-                <input type="search" class="form-control col-md-9 mx-sm-2" name='tag' value="{{$tag}}" placeholder="Tag" required>
-                <button class="btn btn-outline-dark" type="submit" value="Search">Search tag</button>
+            <form action="{{ action('InstagramController@tagSearch') }}" method="get">
+                <div class="form-row">
+                    <div class="form-group col-md-10">
+                        <input type="text" class="form-control" 
+                        name="tag" value="{{$tag}}" placeholder="Tag" required>
+                    </div>
+                    <div class="form-group col-md-2">
+                    <select class="form-control" name="imageCount">
+                        <option value="9" selected>9</option>
+                        <option value="18">18</option>
+                        <option value="30">30</option>
+                        <option value="45">45</option>
+                        <option value="60">60</option>
+                    </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-outline-dark" value="Search">Search tag</button>
             </form>
         </div>
     </section>
 
+
+        {{-- {{var_dump($media)}} --}}
     @if (isset($error) and !empty($tag))
     <section class="font-weight-bold text-center text-muted">
         <p>{{ $error }}</p>
@@ -24,8 +40,19 @@
                     @foreach ($images as $image)
                         <div class="col-md-4">
                             <div class="card mb-4 shadow-sm">
-                                <div class="instagram-image">
-                                    <img src="{{ $image }}">
+
+                                <div class="instagram-image card-img-top">
+                                    <img src="{{ $image['square'] }}">
+                                </div>
+
+                                <div class="card-body">
+                                {{-- <p class="card-text">{{$image['caption']}}</p> --}}
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary"><a href="{{$image['link']}}" target="_blank">View on Instagram<a></button>
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Add to favorites</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div> 
                         </div>
