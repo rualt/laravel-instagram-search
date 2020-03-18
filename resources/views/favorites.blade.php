6e36b0ed7678197c7d17f797a2848fb6da245dde
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-19 02:37:52 +0400

    fix favorites page template

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -1,15 +1,17 @@
 @extends ('layouts.app')
 
+@section ('title', 'Favorites')
+
 @section ('content')
 
 <main role="main">
     <section class="jumbotron text-center">
         <div class="container">
             <h1>Favorites</h1>
-            <p class="lead text-muted">Search instagram by tag</p>
-            <a href="{{ route('index') }}">Go back and search</a>
+            <p class="lead text-muted">Here are all the images you previously saved. Delete some or add them back if you change your mind. </p>
         </div>
     </section>
+
 @if (isset($images))
 <div class="album py-5 bg-light">
     <div class="container">
@@ -23,22 +25,21 @@
                         </div>
                         
                         <div class="card-body">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="button-group d-flex flex-wrap justify-content-center align-items-center" role="group">
-                                    <button type="button" class="btn btn-outline-info btn-sm"><a href="{{$image['page_link']}}" target="_blank">View on Instagram<a></button>
-                                    <form class='galery-update'>
-                                        <input type="hidden" class="source" name="source" value="{{ $image['page_link'] }}">
-                                        <input type="hidden" class="square" name="square" value="{{ $image['square_image'] }}">
-                                        <button type="submit"  formaction="add" class="add btn btn-outline-success btn-sm d-none">Add to
-                                            favorite</button>
-                                        <button type="submit"  formaction="delete" class="delete btn btn-outline-danger btn-sm">Remove from
-                                            favorite</button>                              
-                                    </form>
-                                </div>
+                            <div class="button-group d-flex flex-wrap justify-content-center align-items-center" role="group">
+                                <button type="button" class="btn btn-outline-info btn-sm"><a href="{{$image['page_link']}}" target="_blank">View on Instagram<a></button>
+                            <form class='galery-update'>
+                                <input type="hidden" class="source" name="source" value="{{ $image['page_link'] }}">
+                                <input type="hidden" class="square" name="square" value="{{ $image['square_image'] }}">
+                                 <button type="submit"  formaction="add" class="add btn btn-outline-success btn-sm d-none">Add to
+                                    favorite</button>
+                                <button type="submit"  formaction="delete" class="delete btn btn-outline-danger btn-sm">Remove from
+                                        favorite</button>                              
+                                </form>
                             </div>
                         </div>
-                    </div> 
-                </div>
+                    </div>
+
+                </div> 
             @endforeach
         </div>
     </div>
